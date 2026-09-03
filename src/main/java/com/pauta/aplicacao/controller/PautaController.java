b60f0a70bd2e3ad35177709e66dc6f1d7dc868c8
IngridSantos321 <ingridkitkat2@gmail.com>
2026-09-02 21:14:49 -0300

    testing

--- src/main/java/com/pauta/aplicacao/controller/PautaController.java
+++ src/main/java/com/pauta/aplicacao/controller/PautaController.java
@@ -1,7 +1,5 @@
 package com.pauta.aplicacao.controller;
 
-import com.pauta.aplicacao.model.Pauta;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pauta")
 public class PautaController {
 
-    @GetMapping
-    public Pauta retornarPauta(){
-        Pauta pauta = new Pauta();
-        pauta.setId(1L);
-        pauta.setDescricao("valorstring");
-        return pauta;
-    }
+
 }
