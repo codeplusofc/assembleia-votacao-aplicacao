3562401411c9c6549a2bb6ff86ce2e29d2cc54ae
IngridSantos321 <ingridkitkat2@gmail.com>
2026-09-17 21:39:15 -0300

    criando camada para pauta

--- src/main/java/com/pauta/aplicacao/model/Pauta.java
+++ src/main/java/com/pauta/aplicacao/model/Pauta.java
@@ -1,6 +1,14 @@
 package com.pauta.aplicacao.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Pauta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String descricao;
 
